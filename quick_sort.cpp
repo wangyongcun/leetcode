70968c8338a7ced3ab76c2e92241103ae71d81df
wangyongcun <pingwyc@163.com>
2020-05-19 21:15:36 +0800

    悲哀啊，又忘了，再写一遍又重新发明了一次算法

---
+++ quick_sort.cpp
@@ -0,0 +1,57 @@
+// leetcode.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+void qs(int* nums, int b, int e)
+{
+	if (b >= e)
+	{
+		return;
+	}
+
+	int p = partion(nums, b, e);
+
+	qs(nums, b, p);
+	qs(nums, p + 1, e);
+}
+
+int partion(int* nums,int b,int e)
+{
+	int m = nums[b];
+
+	while (b < e)
+	{
+		if (nums[b] < m)
+		{
+			++b;
+		}
+		else if (nums[e] >= m)
+		{
+			--e;
+		}
+		else if (e > b)
+		{
+			int t = nums[b];
+			nums[b] = nums[e];
+			nums[e] = t;
+		}
+	}
+
+	if (nums[b] > m)
+		b--;
+
+	return b;
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int nums[100] = { 9,8,2,6,4,5,3,1,0,7 };
+	qs(nums, 0, 9);
+
+	for (int i = 0; i <= 9; ++i)
+	{
+		printf("%d", nums[i]);
+	}
+	return 0;
+}
+
