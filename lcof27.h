9c210561a483cb5de3405be883a61702fc699c89
wangyongcun <pingwyc@163.com>
2020-05-21 16:55:58 +0800

    add lcof27

---
+++ lcof27.h
@@ -0,0 +1,37 @@
+#pragma once
+///https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/// easy problem,but I make 2 mistake
+/*
+1 left =0 and right != 0 , first edition donnot swap to make mirror
+2 direct swap value ,not modify pointer lead below node still stay the same side
+*/
+/*
+class Solution {
+public:
+	TreeNode* mirrorTree(TreeNode* root) {
+		if (root)
+		{
+			TreeNode* t = root->left;
+			root->left = root->right;
+			root->right = t;
+
+			if (root->left)
+				mirrorTree(root->left);
+			if (root->right)
+				mirrorTree(root->right);
+		}
+
+		return root;
+	}
+};
+*/
